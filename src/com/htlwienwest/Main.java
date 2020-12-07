ddc34a13d36a019f78383ac40ab599d0691d428e
Ryszka85 <ryszka.a85@htlwienwest.at>
2020-12-07 15:24:41 +0100

    changed greettter interface name

--- src/com/htlwienwest/Main.java
+++ src/com/htlwienwest/Main.java
@@ -7,10 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
         /*System.out.println("hallo fucking world");*/
-        List<Gretter> foo = new ArrayList<>();
+        List<Greeter> foo = new ArrayList<>();
         foo.add(() -> System.out.println("Geh scheissen"));
         foo.add(() -> System.out.println("Du kannst mich mal"));
-        foo.forEach(Gretter::greet);
+        foo.add(() -> System.out.println("Du kannst mich mal 30"));
+        foo.forEach(Greeter::greet);
 
     }
 }
