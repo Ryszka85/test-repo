973071e5daa83685ae249b11089ead94f7d7fcb3
Ryszka85 <ryszka.a85@htlwienwest.at>
2020-12-07 15:36:20 +0100

    changed some text

--- src/com/htlwienwest/Main.java
+++ src/com/htlwienwest/Main.java
@@ -9,7 +9,7 @@ public class Main {
         /*System.out.println("hallo fucking world");*/
         List<Gretter> foo = new ArrayList<>();
         foo.add(() -> System.out.println("Geh scheissen"));
-        foo.add(() -> System.out.println("Du kannst mich mal"));
+        foo.add(() -> System.out.println("Du kannst mich mal 999"));
         foo.forEach(Gretter::greet);
 
     }
