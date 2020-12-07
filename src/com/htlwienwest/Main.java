a2622f4ce28d08540f1fa2ad4291f80d42ae7fc0
Ryszka85 <ryszka.a85@htlwienwest.at>
2020-12-07 15:21:06 +0100

    Feature : added Person class and Greeter interface to greet a lot

--- src/com/htlwienwest/Main.java
+++ src/com/htlwienwest/Main.java
@@ -1,9 +1,16 @@
 package com.htlwienwest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         /*System.out.println("hallo fucking world");*/
-        new Person("Hugo", "Stiglitz").greet();
+        List<Gretter> foo = new ArrayList<>();
+        foo.add(() -> System.out.println("Geh scheissen"));
+        foo.add(() -> System.out.println("Du kannst mich mal"));
+        foo.forEach(Gretter::greet);
+
     }
 }
